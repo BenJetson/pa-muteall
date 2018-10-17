bad9a8720531e8efdf62b613e6a93ac87b7ce773
Ben Godfrey <BenJetson@users.noreply.github.com>
2018-10-17 13:36:40 -0400

    Fix pattern

--- mute-all.sh
+++ mute-all.sh
@@ -2,7 +2,7 @@
 
 # Mutes all PulseAudio sinks connected to the system.
 
-SINKS=$(pacmd list-sinks | grep -e 'index' | grep -Eo '[0-9]{1,4}')
+SINKS=$(pacmd list-sinks | grep -e 'index' | grep -Eo '[0-9]')
 
 for i in $SINKS; do
     pactl set-sink-volume $i 0% || true
